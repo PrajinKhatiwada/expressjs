3685a8df1066f7d0b77900595b61c5039cfcc26d
Prajin Khatiwada <prajinkhatiwada@gmail.com>
2024-05-30 10:48:22 +0545

    Static files served and created own middleware

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const path =require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello everyone ')
-})
-app.get('/about', (req, res) => {
+ //creating middleware
+const myMiddleware=(req,res,next)=>{
+    console.log(req)
+    next()    //next function
+}
+app.use(express.static(path.join(__dirname,"public")))  //serving static folder and files
+//using mdiddleware
+app.use(myMiddleware)
+// app.get('/', (req, res) => {
+  //res.send('Hello everyone ')
+//})
+//app.get('/about', (req, res) => {
    
-    res.sendFile(path.join(__dirname,'index.html'))  //accesing index.html file through server
+    //res.sendFile(path.join(__dirname,'index.html'))  //accesing index.html file through server
    // res.status(500)    //sending costume status code
    // res.json({"Name":"Prajin Khatiwada"})    // sending json file
-  })
+  //})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
